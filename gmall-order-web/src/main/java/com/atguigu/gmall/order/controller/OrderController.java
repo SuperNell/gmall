850df843c65c083820b36e5b1572453c5fa6e086
陈一凡 <chen_yi_fan1@163.com>
2018-07-02 20:42:15 +0800

    谷粒商场环境搭建2018.7.2

---
+++ gmall-order-web/src/main/java/com/atguigu/gmall/order/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.atguigu.gmall.order.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.UserAddress;
+import com.atguigu.gmall.service.UserAddressService;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class OrderController {
+
+    // 调用service 层 服务
+    @Reference
+    private UserAddressService userAddressService;
+
+    // 根据用id 进行查询 ，用户id 应该是传递过来的值
+    // http://localhost:8080/trade?userId=1
+    @RequestMapping("trade")
+    public List<UserAddress> trade(HttpServletRequest request){
+        String userId = request.getParameter("userId");
+        List<UserAddress> userAddressList = userAddressService.getUserAddressList(userId);
+        return  userAddressList;
+
+    }
+
+
+}
